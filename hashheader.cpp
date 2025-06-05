e6a4a1bd7a50cf5f7fba5555a01d82db3e3765bc
western1258 <122416882+western1258@users.noreply.github.com>
2025-06-05 13:07:15 -0600

    hashheader.cpp

---
+++ hashheader.cpp
@@ -0,0 +1,36 @@
+#pragma once 
+#include <iostream>
+#include <string>
+#define CAPACIDAD 10
+using namespace std;
+
+typedef string Clave;
+typedef int Valor;
+typedef string user;
+enum Estado { 
+    VACIO,
+    OCUPADO
+};
+
+struct Celda {
+    Clave clave;
+    user usuario;
+    Valor valor;
+    Estado estado;
+};
+
+class TablaHash {
+private:
+    Celda tabla[CAPACIDAD];
+
+    int funcionHash(const Clave& clave);
+
+public:
+
+    TablaHash() ;
+    //registrar una nueva persona 
+    void Registrar(const string& usuario, const string& clave) ;
+    //iniciar sesion
+    bool iniciarsesion(const string& usuario,const Clave& clave);
+    
+};
