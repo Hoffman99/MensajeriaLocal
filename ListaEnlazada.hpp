391b6c05b3dca857da1c96c7284f09a2d493ef4f
Hoffman99 <zS24013023@estudiantes.uv.mx>
2025-06-06 15:29:11 -0600

    Update ListaEnlazada.hpp

--- ListaEnlazada.hpp
+++ ListaEnlazada.hpp
@@ -1,5 +1,21 @@
 #pragma once
-#include "Nodo.hpp"
+#define TAM 1024
+
+struct Cliente {
+    int socket_fd = -1;
+    char mensaje[TAM] = {0};
+    char usuario[50] = {0};
+    char contraseña[50] = {0};
+    bool autenticado = false;
+    bool estado = false;
+};
+
+class Nodo {
+public:
+    Cliente dato;
+    Nodo* siguiente;
+    Nodo(Cliente valor) : dato(valor), siguiente(nullptr) {}
+};
 
 class Lista {
 private:
